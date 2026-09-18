103f50e172cabb3d11491ea701bfad5af4cd81eb
novaprocurementai-debug <nova.procurement.ai@gmail.com>
2026-09-18 08:47:32 +0300

    Create MainActivity.java

---
+++ app/src/main/java/com/nova/procurement/MainActivity.java
@@ -0,0 +1,153 @@
+package com.nova.procurement;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.net.Uri;
+import android.content.Intent;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.ValueCallback;
+
+public class MainActivity extends Activity {
+
+    private static final String NOVA_URL =
+            "https://nova-procurement.nova-procurement-ai.workers.dev/";
+
+    private WebView webView;
+    private ValueCallback<Uri[]> fileCallback;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+        settings.setAllowContentAccess(true);
+        settings.setAllowFileAccess(false);
+        settings.setSupportZoom(false);
+
+        CookieManager cookies = CookieManager.getInstance();
+        cookies.setAcceptCookie(true);
+        cookies.setAcceptThirdPartyCookies(webView, false);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(
+                    WebView view,
+                    WebResourceRequest request) {
+
+                Uri uri = request.getUrl();
+                String host = uri.getHost();
+
+                if (host != null &&
+                    host.equals("nova-procurement.nova-procurement-ai.workers.dev")) {
+                    return false;
+                }
+
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, uri));
+                } catch (Exception ignored) {
+                }
+
+                return true;
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public boolean onShowFileChooser(
+                    WebView view,
+                    ValueCallback<Uri[]> callback,
+                    FileChooserParams params) {
+
+                if (fileCallback != null) {
+                    fileCallback.onReceiveValue(null);
+                }
+
+                fileCallback = callback;
+
+                try {
+                    startActivityForResult(
+                            params.createIntent(),
+                            1001
+                    );
+                    return true;
+                } catch (Exception e) {
+                    fileCallback = null;
+                    return false;
+                }
+            }
+        });
+
+        if (savedInstanceState == null) {
+            webView.loadUrl(NOVA_URL);
+        } else {
+            webView.restoreState(savedInstanceState);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(
+            int requestCode,
+            int resultCode,
+            Intent data) {
+
+        if (requestCode == 1001 && fileCallback != null) {
+
+            Uri[] results = null;
+
+            if (resultCode == RESULT_OK && data != null) {
+
+                if (data.getClipData() != null) {
+
+                    int count = data.getClipData().getItemCount();
+                    results = new Uri[count];
+
+                    for (int i = 0; i < count; i++) {
+                        results[i] =
+                                data.getClipData()
+                                        .getItemAt(i)
+                                        .getUri();
+                    }
+
+                } else if (data.getData() != null) {
+
+                    results = new Uri[]{
+                            data.getData()
+                    };
+                }
+            }
+
+            fileCallback.onReceiveValue(results);
+            fileCallback = null;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        webView.saveState(outState);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+                    }
