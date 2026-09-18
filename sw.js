4f55691510056a7d806769dc64b824cbdced6964
novaprocurementai-debug <nova.procurement.ai@gmail.com>
2026-09-18 07:44:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,16 @@
-const CACHE_NAME = "nova-pwa-v1";
+const CACHE_NAME = "nova-pwa-v2";
 
 const APP_SHELL = [
   "/",
-  "/NOVA-index-final.html",
   "/manifest.webmanifest",
-  "/nova-icon.svg"
+  "/nova-icon-192.png",
+  "/nova-icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(APP_SHELL))
-      .catch(() => {})
   );
 
   self.skipWaiting();
